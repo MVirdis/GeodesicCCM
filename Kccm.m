8856a8ad4b7ba79a9ab89178e7e7b4b1da34cd26
Mario Virdis <virdis.mario97@gmail.com>
2025-04-16 15:30:39 +0200

    Add a CCM controller evaluation func

---
+++ Kccm.m
@@ -0,0 +1,31 @@
+function u = Kccm(config, dK, xeq,x,xk)
+
+u = {0,xk};
+
+% Find geodesic
+params = geodesicParams(config, xeq, x);
+opts = optimoptions('fmincon','Algorithm','sqp',...
+                    'Display','off', ...
+                    'MaxFunctionEvaluations',1e6);
+[copt,~,flag] = fmincon(params.f, xk, [], [], ...
+                        params.Aeq, params.beq, [], [], [], opts);
+
+% 1  First order optimality conditions satisfied.
+% 0  Too many function evaluations or iterations.
+% -1  Stopped by output/plot function.
+% -2  No feasible point found.
+switch(flag)
+    case 1
+        % Evaluate along geodesic
+        u_ = evalDiffK(config,copt,dK);
+        u = {u_, copt};
+    case 0
+        error('Too many function evaluations or iterations.');
+    case -1
+        error('Stopped by output/plot function.');
+    case -2
+        error('No feasible point found.');
+end
+
+end
+
